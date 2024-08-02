3c4f1befc65346515f6def81fad1a82ec8662b84
renatoxd152 <renatomorillo@gmail.com>
2024-08-02 18:41:47 -0300

    feat: Adicionando Try Catch no backend

--- api/src/routers/RotasNumeros.ts
+++ api/src/routers/RotasNumeros.ts
@@ -1,25 +1,33 @@
 import express from 'express';
 import NumeroRomano from '../models/ConverterRomano';
-const numeros = express.Router()
 
-numeros.post('/numeros/romanos', (req, res) => {
-    const { numero } = req.body;
-    if (numero > 3999 || numero < 1) {
-        return res.status(404).json({romano:"Número inválido"});
+const numeros = express.Router();
+
+numeros.post('/numeros/romanos', async (req, res) => {
+    try {
+        const { numero } = req.body;
+        if (numero > 3999 || numero < 1) {
+            return res.status(400).json({ romano: "Número inválido" });
+        }
+
+        const numeroRomano = new NumeroRomano(numero);
+        const resultado = numeroRomano.converterNumero();
+        res.status(200).json({ romano: resultado });
+    } catch (error) {
+        res.status(500).json({ erro: 'Erro interno do servidor' });
     }
-    
-    const numeroRomano = new NumeroRomano(numero);
-    const resultado = numeroRomano.converterNumero();
-    res.status(200).json({romano: resultado});
 });
 
+numeros.post('/numeros', async (req, res) => {
+    try {
+        const { numero } = req.body;
 
-numeros.post('/numeros', (req, res) => {
-    const { numero } = req.body;
-    
-    const numeroRomano = new NumeroRomano(numero);
-    const resultado = numeroRomano.converterParaNumero()
-    res.status(200).json({numero: resultado});
+        const numeroRomano = new NumeroRomano(numero);
+        const resultado = numeroRomano.converterParaNumero();
+        res.status(200).json({ numero: resultado });
+    } catch (error) {
+        res.status(500).json({ erro: 'Erro interno do servidor' });
+    }
 });
 
 export default numeros;
